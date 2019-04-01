938ee4ded9a8d2a9eb7dc46f0b8f1d250c2a03e2
Peter Wennerstrøm Eriksen <PWE@commentor.dk>
2019-04-01 14:51:59 +0200

    Added lighting demo scene

---
+++ Assets/Scripts/Actors/TestEnemies/TestEnemy1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestEnemy1 : MonoBehaviour
+{
+    public SpriteAnimationFrames_Single Anim;
+
+    SpriteRenderer renderer_;
+
+    private void Awake()
+    {
+        renderer_ = GetComponent<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        renderer_.sprite = SimpleSpriteAnimator.GetAnimationSprite(Anim.Sprites, Anim.DefaultAnimationFramesPerSecond);
+    }
+}
